4c989f9acfa4c34bc99c1c0818f4b9f47328c0ad
Tabatha Roman <romantabatha17@gmail.com>
2020-09-13 01:01:30 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -8,7 +8,7 @@ def key_for_min_value(name_hash)
  lowest_value = nil
   name_hash.each do |key, value|
     if lowest_value == nil || value < lowest_value
-      lowest_value == value 
+      lowest_value = value 
     binding.pry
   end
   end
