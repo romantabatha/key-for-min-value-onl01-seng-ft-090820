e05ec297dcddfef6bb33472a45aed1d6df123b27
Tabatha Roman <romantabatha17@gmail.com>
2020-09-13 00:48:08 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,21 +3,21 @@
 
 require "pry" 
 
-# def key_for_min_value(name_hash)
-#   name_hash.collect do |key, value|
-    
-#   end
-# binding.pry
-# end
-
 def key_for_min_value(name_hash)
-  lowest_key = nil 
-  lowest_value = nil 
-  name_hash.each do |k, v|
-    if lowest_value == nil || v < lowest_value
-      lowest_value = v 
-      lowest_key = k 
-    end 
-  end 
-  lowest_key
-end 
+  name_hash.collect do |key, value|
+    
+  end
+binding.pry
+end
+
+# def key_for_min_value(name_hash)
+#   lowest_key = nil 
+#   lowest_value = nil 
+#   name_hash.each do |k, v|
+#     if lowest_value == nil || v < lowest_value
+#       lowest_value = v 
+#       lowest_key = k 
+#     end 
+#   end 
+#   lowest_key
+# end 
